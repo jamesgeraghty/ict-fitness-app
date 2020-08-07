6f01c7ef93eb10ff6923193f735b141a33412fc3
Glitch (ict-skills-fitness-app) <none>
2020-08-07 13:46:28 +0000

    💃🏥 Updated with Glitch

--- routes.js
+++ routes.js
@@ -9,7 +9,9 @@ const about = require("./controllers/about.js");
 router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
+router.get("/dashboard/deleteassessment/:id", dashboard.deleteAssessment);
 
 router.post("/dashboard/addassessment", dashboard.addAssessment);
+
 
 module.exports = router;
